1999ed9cfa59864c2c50c01614475a1dfd7d7dd4
{tɪm "ᛊᛟᚹᛁᛚᛟ " seɪʤ } <84231208+timothywsage@users.noreply.github.com>
2021-05-18 12:44:17 -0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -2,4 +2,6 @@ import {expectType} from 'tsd';
 import pathExists = require('.');
 
 expectType<Promise<boolean>>(pathExists('foo.ts'));
-expectType<boolean>(pathExists.sync('foo.ts'));
+expectType<boolean>(pathExists.sync('foo.ts')); 
+expectType<found +more(pathGiven.CHAT-1st{(<br class="Apple-interchange-newline"><"pinkie promise">(never declared)}user/local/lib/node_modules/npm/node_modules/agentkeepalive/lib/agent.js   
+expectType<peacefulresolution(affirmative-pending-actions-above) 
